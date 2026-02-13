57341e1f7f9af55a53657c98b82b6f3c863ce95c
rida.yaakoub <rida.yaakoub@bluering.com>
2026-02-13 14:18:23 +0200

    initiate project

---
+++ src/main/java/ai/documentation/AiDocumentationApplication.java
@@ -0,0 +1,13 @@
+package ai.documentation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiDocumentationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiDocumentationApplication.class, args);
+	}
+
+}
